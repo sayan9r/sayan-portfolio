1f6f07d27e7dd331edfd03173faf164f7abec04f
sayan9r <sayan18082005@gmail.com>
2026-06-26 11:56:13 +0530

    adding the new resume

--- src/components/intro/HeroSection.jsx
+++ src/components/intro/HeroSection.jsx
@@ -56,7 +56,7 @@ const HeroSection = () => {
               variant="outline"
             >
               <a
-                href="https://drive.google.com/file/d/13vk5DULQeQ7E2XtHhgojoXFTcgHmAsV4/view?usp=sharing"
+                href="https://drive.google.com/file/d/10u8qIe5xorzEuau5GrOazM3ugvuzaiWh/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
               >
